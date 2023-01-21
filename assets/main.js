0e6436a94346b2a7e3194d068140842bf7c1079e
aep <aep@qwords.co.id>
2023-01-21 13:14:01 +0700

    insert and delete

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,66 @@
+
+var items = {
+    datas : []
+}
+
+var current = {
+    item:null
+}
+
+var loc_item = localStorage.getItem('items')
+
+if (Boolean(loc_item)){
+    items = JSON.parse(loc_item)
+}
+
+var myModalEl = document.getElementById('myModal')
+var modal = bootstrap.Modal.getOrCreateInstance(myModalEl) 
+
+var modalInsertEl = document.getElementById('modal-insert')
+var modalInsert = bootstrap.Modal.getOrCreateInstance(modalInsertEl)
+
 var swiper = new Swiper(".mySwiper", {});
 
-// Get a reference to an element
-var square = document.querySelector('body');
-
-// Create a manager to manager the element
-var manager = new Hammer.Manager(square);
-
-// Create a recognizer
-var DoubleTap = new Hammer.Tap({
-    event: 'doubletap',
-    taps: 2
-});
-
-// Add the recognizer to the manager
-manager.add(DoubleTap);
-
-// Subscribe to desired event
-manager.on('doubletap', function (e) {
-    alert('oi')
+var square = document.querySelector('.mySwiper');
+square.addEventListener('dblclick', (event) => {
+    modalInsert.show()
 });
 
 
 $('#slide1').on('click', 'span',function(){
-    // alert(this.innerText)
+
+    current.item = this.innerText
     modal.show()
 })
 
-var myModalEl = document.getElementById('myModal')
-var modal = bootstrap.Modal.getOrCreateInstance(myModalEl) 
+$('#insert-btn').click(function(){
+    const item = $('#input-name').val()
+    items.datas.push({name:item})
+        
+    localStorage.setItem('items', JSON.stringify(items));
+    renderHome();
+    modalInsert.hide()
+})
+
+
+$('#delete-btn').click(function(){
+    
+    const filtered = items.datas.filter(v => v.name !== current.item.replace('#',''))
+    console.log(filtered, current.item)
+    items.datas = filtered
+    localStorage.setItem('items', JSON.stringify(items));
+    renderHome();
+    modal.hide()
+
+})
+
+function renderHome(){
+
+    $('#item-container').empty();
+    items.datas.forEach(el => {
+        $('#item-container').append(`<span class="badge rounded-pill bg-primary fs-3">#${el.name}</span>`);
+    })
+
+}
+
+renderHome();
